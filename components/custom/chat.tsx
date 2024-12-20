f233424c7d19baf6e81ccc7ca43d1cf048574ea3
Jacob Ambroz <143131645+sixtyoneeighty@users.noreply.github.com>
2024-12-20 11:31:19 -0600

    d

---
+++ components/custom/chat.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import { Attachment, Message } from "ai";
+import { useChat } from "ai/react";
+import { useState } from "react";
+
+import { Message as PreviewMessage } from "@/components/custom/message";
+import { useScrollToBottom } from "@/components/custom/use-scroll-to-bottom";
+import Image from "next/image";
+
+import { MultimodalInput } from "./multimodal-input";
+import { Overview } from "./overview";
+
+export function Chat({
+  id,
+  initialMessages,
+}: {
+  id: string;
+  initialMessages: Array<Message>;
+}) {
+  const { messages, handleSubmit, input, setInput, append, isLoading, stop } =
+    useChat({
+      id,
+      body: { id },
+      initialMessages,
+      maxSteps: 10,
+      onFinish: () => {
+        window.history.replaceState({}, "", `/chat/${id}`);
+      },
+    });
+
+  const [messagesContainerRef, messagesEndRef] =
+    useScrollToBottom<HTMLDivElement>();
+
+  const [attachments, setAttachments] = useState<Array<Attachment>>([]);
+
+  return (
+    <div className="flex flex-row justify-center pb-4 md:pb-8 h-dvh bg-background">
+      <div className="flex flex-col justify-between items-center gap-4 w-full relative">
+        <div className="absolute inset-0 flex items-center justify-center pointer-events-none select-none">
+          <Image
+            src="/images/prcb.png"
+            alt="Chat Background"
+            width={600}
+            height={600}
+            className="opacity-15"
+          />
+        </div>
+        <div
+          ref={messagesContainerRef}
+          className="flex flex-col gap-4 h-full w-dvw items-center overflow-y-scroll relative"
+        >
+          {messages.length === 0 && <Overview />}
+
+          {messages.map((message) => (
+            <PreviewMessage
+              key={message.id}
+              chatId={id}
+              role={message.role}
+              content={message.content}
+              attachments={message.experimental_attachments}
+            />
+          ))}
+
+          <div
+            ref={messagesEndRef}
+            className="shrink-0 min-w-[24px] min-h-[24px]"
+          />
+        </div>
+
+        <div className="flex flex-row gap-2 relative items-end w-full md:max-w-[800px] max-w-[calc(100dvw-32px)] px-4 md:px-0">
+          <MultimodalInput
+            input={input}
+            setInput={setInput}
+            handleSubmit={handleSubmit}
+            isLoading={isLoading}
+            stop={stop}
+            attachments={attachments}
+            setAttachments={setAttachments}
+            messages={messages}
+            append={append}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
--- lib/drizzle/0001_large_stingray.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "Chat" ALTER COLUMN "createdAt" SET DEFAULT now();--> statement-breakpoint
-ALTER TABLE "Chat" ALTER COLUMN "messages" SET DEFAULT '[]'::json;
